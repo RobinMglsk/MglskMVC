250bc77dd4eed1adb097726aea43ebb5bf2f8d1c
RobinMglsk <robin@migalski.be>
2018-01-07 20:42:09 +0100

    Added folders to folder structure and made a logo.

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,5 +1,7 @@
 <?php require(APPROOT . "/views/inc/headers.php"); ?>
 
-<h1><?php echo $data['title']; ?></h1>
-<p>This is the MglskMVC PHP Framework.
+    <h1><?php echo $data['title']; ?></h1>
+    <p>This is the MglskMVC PHP Framework.</p>
+    <img scr="/img/logo.png" alt="MglskMVC">
+    
 <?php require(APPROOT . "/views/inc/footer.php"); ?>
